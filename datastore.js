a84362cd288ce7c671d2fb23a5e639ecf1424871
Hardeep Dhaliwal <hdhaliwal@thinkful.com>
2018-07-07 07:46:35 -0700

    second commit

---
+++ datastore.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+      question: 'What is one of the most important principles to follow when planning a route?',
+      answers: [
+        'Distractions nature has to offer',
+        'Depends on personal preferences',
+        'Distance from point A to B',
+        'Its for fun, so lose the plan and enjoy sightseeing'
+        ],
+      correctAnswer: 'Distance from point A to B',
+      icon: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2013/12/bugatti-wallpaper-4.jpg',
+      alt: 'bugati icon'
+    },
+    {
+      question: 'How many hours of sleep is recommended?',
+      answers: [
+        'I am superman, no sleep for me',
+        '8 hours to 9 hours',
+        '2 hours',
+        'as i will feel sleeply, will park by the road'
+        ],
+        correctAnswer: '8 hours to 9 hours',
+        icon: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2013/12/Bugatti-wallpaper-6.jpg',
+        alt: 'super cool car'
+    },
+    {
+      question: 'When going for long drive, how important it is to make your car comfortable?',
+      answers: [
+        'Not at all needed',
+        'I will only check the air pressure',
+        'Put some scent in car',
+        'Wash my car|take it for servicing|check the fluids|get it washed'
+        ],
+        correctAnswer: 'Wash my car|take it for servicing|check the fluids|get it washed',
+        icon: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2013/12/Bugatti-wallpaper-10.jpg',
+        alt: 'cool car'
+    },
+    {
+      question: 'You are going for long drive in bear country. How should you store your food?',
+      answers: [
+        'Bears are a myth. There\'s no need to worry about them',
+        'Always store your food on you person at all times.',
+        'Store your food in your tent',
+        'Always use a bear canistern or hang your food and personal hygiene items on a tree limb in a bear backpacking'
+        ],
+      correctAnswer: 'Always use a bear canistern or hang your food and personal hygiene items on a tree limb in a bear backpacking',
+      icon: 'https://i.imgur.com/i0Q515j.png',
+      alt: 'bear paw icon'
+    },
+    {
+      question: 'What you should never day drive without?',
+      answers: [
+        'Extra food',
+        'Hat',
+        'GPS',
+        'Cell phone'
+        ],
+      correctAnswer: 'Extra food',
+      icon: 'https://i.imgur.com/LHRMcBd.png',
+      alt: 'hat icon'
+    },
+    {
+      question: 'You forgot your water filter in the backcountry. What should you do?',
+      answers: [
+        'Just drink straight from the stream',
+        'Drink your own urine',
+        'Find a water source and boil the water to neutralize it',
+        'Don\'t drink any water'
+        ],
+      correctAnswer: 'Find a water source and boil the water to neutralize it',
+      icon: 'https://i.imgur.com/XEwf3tK.png',
+      alt: 'water bottle icon'
+    },
+    {
+      question: 'What is the safest way to cross a stream while driving?',
+      answers: [
+      'Find flat rocks to hop across on',
+      'Look for the deepest section and cross there',
+      'Avoid acrossing where the current is strong. Look for a straight, wide, and shallow section.',
+      'Always follow the marked route no matter what'
+      ],
+      correctAnswer: 'Avoid acrossing where the current is strong. Look for a straight, wide, and shallow section.',
+      icon: 'https://i.imgur.com/Kk9t3qL.png',
+      alt: 'stream icon'
+    },
+    {
+      question: 'While driving you get lost. What should you do now?',
+      answers: [
+        'Stay put and wait for someone to find you',
+        'Follow streams or rivers. They will eventually lead to roads',
+        'Take off driving in one direction. you will eventually hit a trail.',
+        'Eat all of your food now to get the most energy that you can.'
+      ],
+      correctAnswer: 'Stay put and wait for someone to find you',
+      icon: 'https://i.imgur.com/A2cWS88.png',
+      alt: 'compass icon'
+    },
+    {
+      question: 'You just spotted a black bear while driving. What should you do now?',
+      answers: [
+      'Drive slow',
+      'Stop',
+      'Approach the bear. It might want to be friends with you!',
+      'If you spot the bear before it spots you, back away keeping your eyes on the bear and wait for it to pass before continuing on.'
+      ],
+      correctAnswer: 'Drive slow',
+      icon: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2013/12/Bugatti-wallpaper-13.jpg',
+      alt: 'cool car'
+    },
+    {
+      question: 'What is the most dangerous animal in the wilderness?',
+      answers: [
+        'Grizzly bears',
+        'Birds',
+        'Insects',
+        'Humanbeings'
+    ],
+    correctAnswer: 'Humanbeings',
+    icon: 'http://cdn.wonderfulengineering.com/wp-content/uploads/2013/12/Bugatti-wallpaper-14.jpg',
+    alt: 'cool icon'
+    }
+];
